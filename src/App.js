bc575759b10f2541219c2fcac6d1929f8271f1d8
Atishay Jain <hritikjain707@gmail.com>
2021-06-02 08:36:30 +0530

    Feat: Error Page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
   Subscribed,
   LikedVideos,
 } from "./Components";
+import PageNotFound from "./Components/404/PageNotFound";
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
           <Route exact path="/signin" element={<SignIn />} />
 
           <Route exact path="/signup" element={<SignUp />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </div>
       <Sidebar />
